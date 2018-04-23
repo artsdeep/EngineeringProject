45117e7bf55fe1890e7459bd2cdddc3b54a96ef3
artsdeep <artsdeep@gmail.com>
2018-04-23 21:45:53 +0300

    sql

--- app/__init__.py
+++ app/__init__.py
@@ -25,11 +25,47 @@ app.register_blueprint(feature_request)
 
 new_db = db.create_all()
 from app.request.models import Client
+# Import flask and template operators
+from flask import Flask, render_template
+
+# Import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+# Define the WSGI application object
+app = Flask(__name__)
+
+# Configurations
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+from app.request.controllers import feature_request
+app.register_blueprint(feature_request)
+
+db.create_all()
+path_sql_script = app.config.get("BASE_DIR")
+from app.request.models import Client
+
 if db.session.query(Client).count() == 0:
-    db.engine.execute("insert into client(id, name) values (1, 'Client A')")
-    db.engine.execute("insert into client(id, name) values (2, 'Client B')")
-    db.engine.execute("insert into client(id, name) values (3, 'Client C')")
-    db.engine.execute("insert into product_area(id, name) values (1, 'Policies')")
-    db.engine.execute("insert into product_area(id, name) values (2, 'Billing')")
-    db.engine.execute("insert into product_area(id, name) values (3, 'Claims')")
-    db.engine.execute("insert into product_area(id, name) values (4, 'Reports')")
+    sql_command = ''
+    sql_file = open(path_sql_script+"/app/db.sql", 'r')
+    for line in sql_file:
+        if not line.startswith('--') and line.strip('\n'):
+            sql_command += line.strip('\n')
+            if sql_command.endswith(';'):
+                try:
+                    db.engine.execute(str(sql_command))
+
+                    # Assert in case of error
+                except:
+                    print('Ops')
+
+                finally:
+                    sql_command = ''
---
+++ app/db.sql
@@ -0,0 +1,7 @@
+insert into client(id, name) values (1, 'Client A');
+insert into client(id, name) values (2, 'Client B');
+insert into client(id, name) values (3, 'Client C');
+insert into product_area(id, name) values (1, 'Policies');
+insert into product_area(id, name) values (2, 'Billing');
+insert into product_area(id, name) values (3, 'Claims');
+insert into product_area(id, name) values (4, 'Reports');
