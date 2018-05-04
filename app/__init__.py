1f7d980892c2f2f4630600926fadfdc1eb320566
artsdeep <artsdeep@gmail.com>
2018-05-04 10:37:57 +0300

    change

--- app/__init__.py
+++ app/__init__.py
@@ -10,14 +10,13 @@ import os
 app = Flask(__name__, instance_relative_config=True)
 
 # Configurations
-#app.config.from_object('config')
+app.config.from_object('config')
+print(app.config['POSTGRES_URL']+"!!!")
 POSTGRES_URL = "127.0.0.1:5432"
 POSTGRES_USER = "app_user"
 POSTGRES_PW = "app_user_pass"
 POSTGRES_DB = "app"
 DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
-
-
 SQLALCHEMY_DATABASE_URI = DB_URL
 
 db = SQLAlchemy(app)
