b9fb4e2efd044d31b75baba733a9cb9ea4a2283c
artsdeep <artsdeep@gmail.com>
2018-05-04 14:23:46 +0300

    change

--- app/__init__.py
+++ app/__init__.py
@@ -9,15 +9,17 @@ import os
 # Define the WSGI application object
 app = Flask(__name__, instance_relative_config=True)
 
-# Configurations
+# Configurations no worked with heroku
 #app.config.from_object('config')
-#print(app.config['POSTGRES_URL']+"!!!")
+
 POSTGRES_URL = "127.0.0.1:5432"
 POSTGRES_USER = "app_user"
 POSTGRES_PW = "app_user_pass"
 POSTGRES_DB = "app"
 DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
 SQLALCHEMY_DATABASE_URI = DB_URL
+DATABASE_CONNECT_OPTIONS = {}
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
